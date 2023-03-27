aac8336fcec010b8c8bb9c898b6d55eff46b375c
beatrice <helgapaul389@gmail.com>
2023-03-27 12:11:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+const h1_text=document.createTextNode('Beatrice is the champion' )
+newHeader.appendChild(h1_text)
+document.body.append('h1')
